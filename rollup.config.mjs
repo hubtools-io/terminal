f00aee97f16af87b1848e1fc7f9631d67a5f076e
Markie Arnold <markiearnold@gmail.com>
2023-02-18 13:35:43 -0600

    .

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,9 @@
+/* eslint-disable object-curly-spacing */
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json" assert {type: "json"};
+import {terser} from "rollup-plugin-terser";
 import typescriptModule from "typescript";
+import pkg from "./package.json" assert {type: "json"};
 
 export default {
   input: "src/index.tsx",
@@ -16,21 +17,21 @@ export default {
       file: pkg.module,
       format: "es",
       exports: "named",
-    }
+    },
   ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {})
+    ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
     postcss({
       extract: false,
       modules: true,
-      use: ["sass"]
+      use: ["sass"],
     }),
     typescript({
-      typescript: typescriptModule
+      typescript: typescriptModule,
     }),
-    process.env.NODE_ENV === "production" ? terser() : null
-  ]
+    process.env.NODE_ENV === "production" ? terser() : null,
+  ],
 };
